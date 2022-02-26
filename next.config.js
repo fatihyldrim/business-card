ff1e3747a79c55a9d26b793770bc7645b8df1782
FATİH YILDIRIM <21792018+fatihyldrim@users.noreply.github.com>
2022-02-27 00:14:21 +0300

    next fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/nf-nft/business-card/' : ''
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
